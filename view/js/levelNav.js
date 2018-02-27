b1cc67912e694a636d027aeb1854cb43922336fa
Med Said <mohamedsaidmbareck@gmail.com>
2018-02-27 05:10:57 -0800

    start matiere

--- view/js/levelNav.js
+++ view/js/levelNav.js
@@ -1,5 +1,4 @@
 
-
 //affichage du semester lorsqu'on click sur le filiere
 
 var filieres = document.getElementsByClassName('filiere');
@@ -7,10 +6,11 @@ var i,j;
 for(i = 0,j = filieres.length; i < j; i++)
 {
 	filieres[i].addEventListener('click',function (e) {
-		//e: click, currentTarget: td , parentNode: tr
+	//e: click, currentTarget: td , nextElementSibling: sOne
+	var sOne = e.currentTarget.nextElementSibling;
+	var sTwo = e.currentTarget.previousElementSibling;
 
-		alert(e.currentTarget.nextElementSibling);
+	sOne.style.visibility ='initial';
+	sTwo.style.visibility ='initial';
 	})
 }
-
-
